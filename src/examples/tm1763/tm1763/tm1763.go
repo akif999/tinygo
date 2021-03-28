d50aa13c1d40d9d3333baf766887d879d01f2bc0
akif999 <aki01nf28@gmail.com>
2021-03-28 23:28:22 +0900

    progressed implementation

--- src/examples/tm1763/tm1763/tm1763.go
+++ src/examples/tm1763/tm1763/tm1763.go
@@ -6,12 +6,13 @@ import (
 )
 
 type device struct {
-	Clk      machine.Pin
-	Dio      machine.Pin
-	Data     uint8
-	Addr     uint8
-	dispCtrl uint8
-	point    bool
+	Clk        machine.Pin
+	Dio        machine.Pin
+	Data       uint8
+	Addr       uint8
+	dispCtrl   uint8
+	point      bool
+	brightness uint8
 }
 
 func New(clk, dio uint8) *device {
@@ -33,6 +34,8 @@ func (d *device) Set(data, addr, brightness uint8) {
 	d.brightness = 0x88 + brightness
 }
 func (d *device) Display() {}
+
+func (d *device) DisplayWithBitAddr() {}
 
 func (d *device) Point(point bool) {
 	d.point = point
@@ -56,7 +59,30 @@ func (d *device) stop() {
 	d.Dio.High()
 }
 
-func (d *device) coding() {}
+func (d *device) coding(dispData []uint8) {
+	for i := 0; i < 4; i++ {
+		_ = d._coding(&dispData[i])
+	}
+}
+
+func (d *device) _coding(dispData *uint8) uint8 {
+	tubeTab := []uint8{
+		0x3f, 0x06, 0x5b, 0x4f,
+		0x66, 0x6d, 0x7d, 0x07,
+		0x7f, 0x6f, 0x77, 0x7c,
+		0x39, 0x5e, 0x79, 0x71,
+	} //0~9,A,b,C,d,E,F
+	pointData := uint8(0x00)
+	if d.point {
+		pointData = 0x80
+	}
+	if *dispData == 0x7F {
+		*dispData = 0x00 + pointData
+	} else {
+		*dispData = tubeTab[*dispData] + pointData
+	}
+	return *dispData
+}
 
 func bitDelay() {
 	time.Sleep(time.Microsecond * 50)
