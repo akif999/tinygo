47d3fb254e0cee157cb27d5aa0d7221683906018
akif999 <aki01nf28@gmail.com>
2021-04-05 12:15:59 +0900

    implement relay module sample

--- src/examples/tm1637/main.go
+++ src/examples/tm1637/main.go
@@ -8,24 +8,174 @@ import (
 )
 
 func main() {
+	// Initial TM1637
 	device := tm1637.New(machine.ADC5, machine.ADC4)
 	device.Configure()
 	device.Init()
+	device.Set(0x40, 0xC0, 0x02)
 
+	// Initial LED
 	led := machine.LED
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	// Initial siwtches
+	swGrn := machine.D4
+	swGrn.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	swBlu := machine.D0
+	swBlu.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	swBlk := machine.D2
+	swBlk.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	swWht := machine.D3
+	swWht.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	swRed := machine.D5
+	swRed.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+
+	//Initial digital out port
+	relaySwitchOut := machine.D6
+	relaySwitchOut.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	device.Point(true)
+	// 10ms
+	device.DisplayWithBitAddr(0, 1)
+	device.DisplayWithBitAddr(1, 0)
+	// 5times
+	device.DisplayWithBitAddr(2, 0)
+	device.DisplayWithBitAddr(3, 5)
+
+	segParam := struct {
+		energizationTime  uint8 // lsb: 10ms
+		numOfEnergization uint8
+		editingDigit      uint8
+	}{
+		10,
+		5,
+		0,
+	}
 	for {
 		led.Low()
-		device.Point(true)
-		device.DisplayWithBitAddr(0, 2)
-		device.DisplayWithBitAddr(1, 3)
-		device.DisplayWithBitAddr(2, 5)
-		device.DisplayWithBitAddr(3, 5)
-		time.Sleep(time.Millisecond * 500)
+
+		device.DisplayWithBitAddr(0, segParam.energizationTime/10)
+		device.DisplayWithBitAddr(1, segParam.energizationTime%10)
+		device.DisplayWithBitAddr(2, segParam.numOfEnergization/10)
+		device.DisplayWithBitAddr(3, segParam.numOfEnergization%10)
+
+		time.Sleep(time.Millisecond * 200)
 
 		led.High()
-		device.Point(false)
-		device.ClearDisplay()
-		time.Sleep(time.Millisecond * 500)
+
+		device.ClearDisplayWithBitAddr(segParam.editingDigit)
+
+		time.Sleep(time.Millisecond * 200)
+
+		// shift flashing digti right
+		if !swBlu.Get() {
+			if segParam.editingDigit < 3 {
+				segParam.editingDigit++
+			} else if segParam.editingDigit == 3 {
+				segParam.editingDigit = 0x00
+			} else {
+				// nothing to do
+			}
+		}
+		// shift flashing digti left
+		if !swBlk.Get() {
+			if 0 < segParam.editingDigit {
+				segParam.editingDigit--
+			} else if segParam.editingDigit == 0 {
+				segParam.editingDigit = 0x03
+			} else {
+				// nothing to do
+			}
+		}
+
+		// increment prameter
+		if !swWht.Get() {
+			switch segParam.editingDigit {
+			case 0x00:
+				if segParam.energizationTime < 90 {
+					segParam.energizationTime += 10
+				} else if 90 <= segParam.energizationTime {
+					segParam.energizationTime -= 90
+				} else {
+					// nothing to do
+				}
+			case 0x01:
+				if segParam.energizationTime < 99 {
+					segParam.energizationTime++
+				} else if segParam.energizationTime == 99 {
+					segParam.energizationTime = 0
+				} else {
+					// nothing to do
+				}
+			case 0x02:
+				// never editied
+			case 0x03:
+				if segParam.numOfEnergization < 9 {
+					segParam.numOfEnergization++
+				} else if segParam.numOfEnergization == 9 {
+					segParam.numOfEnergization = 0
+				} else {
+					// nothing to do
+				}
+			default:
+				panic("invalid digit")
+			}
+		}
+
+		// decrement prameter
+		if !swGrn.Get() {
+			switch segParam.editingDigit {
+			case 0x00:
+				if 9 < segParam.energizationTime {
+					segParam.energizationTime -= 10
+				} else if segParam.energizationTime <= 9 {
+					segParam.energizationTime += 90
+				} else {
+					// nothing to do
+				}
+			case 0x01:
+				if 0 < segParam.energizationTime {
+					segParam.energizationTime--
+				} else if segParam.energizationTime == 0 {
+					segParam.energizationTime = 99
+				} else {
+					// nothing to do
+				}
+			case 0x02:
+				// never editied
+			case 0x03:
+				if 0 < segParam.numOfEnergization {
+					segParam.numOfEnergization--
+				} else if segParam.numOfEnergization == 0 {
+					segParam.numOfEnergization = 9
+				} else {
+					// nothing to do
+				}
+			default:
+				panic("invalid digit")
+			}
+		}
+
+		// energize
+		if !swRed.Get() {
+			// param 0 is max number of parameter
+			t := uint16(segParam.energizationTime*10 + 15) //15ms is adjustment
+			n := segParam.numOfEnergization
+			if t == 0 {
+				t = 1000
+			}
+			for i := uint8(0); i < n; i++ {
+				time.Sleep(time.Millisecond * time.Duration(t))
+				relaySwitchOut.High()
+				time.Sleep(time.Millisecond * time.Duration(t))
+				relaySwitchOut.Low()
+			}
+		}
+
+		// println(swGrn.Get())
+		// println(swBlu.Get())
+		// println(swBlk.Get())
+		// println(swWht.Get())
+		// println(swRed.Get())
 	}
 }
